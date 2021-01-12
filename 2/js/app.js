531bc18ce8206f5576228fba183954a6325022ae
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-12 17:03:38 +0100

    meteo2: réoganisation du code

--- 2/js/app.js
+++ 2/js/app.js
@@ -12,43 +12,19 @@ const APIKEY = '94c6cf0868fa5cb930a5e2d71baf0dbf';
 console.log("salut")
 function main() {
     // Trouver l'adress Ip de l'utilisateur
-    fetch('https://api.ipify.org?format=json')
-        .then(resultat => resultat.json())
-        .then(json => {
-            const ip = json.ip;
-
-            fetch('https://freegeoip.live/json/' + ip)
+    const ip = fetch('https://api.ipify.org?format=json')
                 .then(resultat => resultat.json())
-                .then(json => {
-                    const ville = json.city;
+                .then(json => json.ip);
 
-                    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville},fr&appid=${APIKEY}&units=metric&lang=fr`)
-                        .then(resultat => resultat.json())
-                        .then(json => {
-                            console.log(json);
-            })
-        })
-    })
+    // Trouver la ville de l'utilisateur
+    const ville = fetch('https://freegeoip.live/json/' + ip)
+                    .then(resultat => resultat.json())
+                    .then(json => json.city);
+
+    // Trouver la météo de l'utilisateur
+    const meteo = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville},fr&appid=${APIKEY}&units=metric&lang=fr`)
+                .then(resultat => resultat.json())
+                .then(json => json.city);
+
 }
 
-
-
-
-
-// fetch('https://api.ipify.org?format=json')
-// .then(resultat => resultat.json())
-// .then(json => {
-//     console.log(json.ip);
-// })
-
-// fetch('https://freegeoip.live/json/' + "81.57.53.204")
-// .then(resultat => resultat.json())
-// .then(json => {
-//     console.log(json.city);
-// })
-
-// fetch(`https://api.openweathermap.org/data/2.5/weather?q=Marseille,fr&appid=${APIKEY}&units=metric&lang=fr`)
-// .then(resultat => resultat.json())
-// .then(json => {
-//     console.log(json);
-// })
