384dfbe3b6d226bf1e0e44175766a87151bcd87a
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-13 21:51:21 +0100

    meteo3: amélioration d'affichage

--- 3/js/app.js
+++ 3/js/app.js
@@ -8,6 +8,10 @@ const weatherIcons = {
   "Drizzle": "wi wi-day-sleet",
 }
 
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
 // fonction d'appel à l'API OpenWeather
 let apiCall = function(city) {
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${city},fr&appid=${APIKEY}&units=metric&lang=fr`;
@@ -16,19 +20,19 @@ let apiCall = function(city) {
         .then((response) =>
             response.json().then((data) => {
                 console.log(data);
-                document.querySelector('#city').innerHTML = data.name;
+                //document.querySelector('#city').innerHTML = data.name;
+                document.querySelector('#general').innerHTML = data.name + ' : ' + '<br/>' + capitalize(data.weather[0].description);
                 document.querySelector('#temp').innerHTML =
-                  "<i class='fas fa-thermometer-half'></i>" + data.main.temp + ' °C';
-                document.querySelector('#sky').innerHTML =
-                  "<i class='fas fa-wind'></i>" + data.weather[0].description;
-                document.querySelector('#wind').innerHTML =
-                  "<i class='fas fa-wind'></i>" + data.wind.speed + ' km/h';
+                  "<i class='fas fa-thermometer-half'></i>" + 'Température : '  + '<br/>' +  + data.main.temp + ' °C';
+                // document.querySelector('#sky').innerHTML =
+                //   "<i class='fas fa-wind'></i>" + data.weather[0].description;
+                document.querySelector('#wind').innerHTML = "<i class='fas fa-wind'></i>" + 'Vent : '  + '<br/>' + data.wind.speed + ' km/h';
                 document.querySelector('#humidity').innerHTML =
-                  "<i class='fas fa-tint'></i>" + data.main.humidity + ' %';
+                  "<i class='fas fa-tint'></i>" + 'Humidité : '  + '<br/>' +  + data.main.humidity + ' %';
                 document.getElementById('img').setAttribute('src',"http://openweathermap.org/img/wn/" + data.weather[0].icon + '@2x.png');
                 //optionnal
                 document.querySelector('#pressure').innerHTML =
-                  "<i class='fas fa-thermometer-full'></i>" + (data.main.pressure)/1000 + ' bar';
+                  "<i class='fas fa-thermometer-full'></i>" + 'Pression : '  + '<br/>' + (data.main.pressure)/1000 + ' bar';
 
                 let sunriseDate = new Date(data.sys.sunrise*1000);
                 let sunriseLocale = sunriseDate.toLocaleString('fr-FR',{
@@ -36,7 +40,7 @@ let apiCall = function(city) {
                     minute: 'numeric',
                     second: 'numeric'});
                 document.querySelector('#sunrise').innerHTML =
-                    "<i class='fas fa-sun'></i>" + sunriseLocale;
+                    "<i class='fas fa-sun'></i>" + "Lever du soleil : "  + '<br/>' + sunriseLocale;
 
                 let sunsetDate = new Date(data.sys.sunset*1000);
                 let sunsetLocale = sunsetDate.toLocaleString('fr-FR',{
@@ -44,7 +48,7 @@ let apiCall = function(city) {
                     minute: 'numeric',
                     second: 'numeric'});
                 document.querySelector('#sunset').innerHTML =
-                  "<i class='far fa-moon'></i>" + sunsetLocale;
+                  "<i class='far fa-moon'></i>" + 'Coucher du soleil : '  + '<br/>' + sunsetLocale;
         })
     )
     .catch((err) => console.log('Erreur : ' + err));
