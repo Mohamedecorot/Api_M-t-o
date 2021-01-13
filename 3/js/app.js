30bbfeddb92c3cbe02373e825b60c3f0d8d4ca4c
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-13 17:14:46 +0100

    meteo3: recuperation et affichage des donnees demandees et ajout des icons

--- 3/js/app.js
+++ 3/js/app.js
@@ -20,17 +20,31 @@ let apiCall = function(city) {
                 document.querySelector('#temp').innerHTML =
                   "<i class='fas fa-thermometer-half'></i>" + data.main.temp + ' °C';
                 document.querySelector('#sky').innerHTML =
-                "<i class='fas fa-wind'></i>" + data.weather[0].description;
+                  "<i class='fas fa-wind'></i>" + data.weather[0].description;
                 document.querySelector('#wind').innerHTML =
-                "<i class='fas fa-wind'></i>" + data.wind.speed + ' km/h';
+                  "<i class='fas fa-wind'></i>" + data.wind.speed + ' km/h';
                 document.querySelector('#humidity').innerHTML =
                   "<i class='fas fa-tint'></i>" + data.main.humidity + ' %';
-
                 document.getElementById('img').setAttribute('src',"http://openweathermap.org/img/wn/" + data.weather[0].icon + '@2x.png');
+                //optionnal
+                document.querySelector('#pressure').innerHTML =
+                  "<i class='fas fa-thermometer-full'></i>" + (data.main.pressure)/1000 + ' bar';
 
-                // document.getElementById('image').src ==
-                //   "http://openweathermap.org/img/wn/" + data.weather[0].icon + '@2x.png';
-              console.log("http://openweathermap.org/img/wn/" + data.weather[0].icon + '@2x.png')
+                let sunriseDate = new Date(data.sys.sunrise*1000);
+                let sunriseLocale = sunriseDate.toLocaleString('fr-FR',{
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric'});
+                document.querySelector('#sunrise').innerHTML =
+                    "<i class='fas fa-sun'></i>" + sunriseLocale;
+
+                let sunsetDate = new Date(data.sys.sunset*1000);
+                let sunsetLocale = sunsetDate.toLocaleString('fr-FR',{
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric'});
+                document.querySelector('#sunset').innerHTML =
+                  "<i class='far fa-moon'></i>" + sunsetLocale;
         })
     )
     .catch((err) => console.log('Erreur : ' + err));
